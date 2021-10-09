beb1541cacd8c85756a49299e06b5b6738f2ffcf
DESKTOP-1HAFD14\deko_ <deko_golab@hotmail.com>
2021-10-09 22:44:46 +0200

    almost

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
   <div id="app">
-    
+        <div class="header">
+            <div class="main_head">
+                <h3>TakeLessons</h3>
+            </div>
+            
+            
+            <div>
+                <select name="" id="navDropdown"> 
+                    <option value=""> Jason.H</option>
+                    <option value="">Jason.S</option>
+                </select> 
+            </div>     
+        </div>
+   
+        <div class="tab">
+          <ul>
+            <li>
+              <button  @click="setActive('dashboard')" :class="{ active: isActive('dashboard') }" href="#">DASHBOARD</button>
+            </li>
+            <li>
+              <button @click="setActive('calendar')" :class="{ active: isActive('calendar') }" href="#">CALENDAR</button>
+            </li>
+            <li>
+              <button @click="setActive('clients')" :class="{ active: isActive('clients') }" href="#">CLIENTS</button>
+            </li>
+            <li>
+              <button @click="setActive('finances')" :class="{ active: isActive('finances') }" href="#">FINANCES</button>
+            </li>
+          </ul>
+        </div>
 
+        <div id="myTabContent">
+          <div class="none" :class="{ 'active': isActive('dashboard') }" id="dashboard">Dashboard component</div>
+          <div class="none" :class="{ 'active': isActive('calendar') }" id="calendar">Calendar component</div>
+          <div class="none" :class="{ 'active': isActive('clients') }" id="clients">    <Main/> </div>
+          <div class="none" :class="{ 'active': isActive('finances') }" id="finances">Finances component</div>
+        </div>
 
-    
-    <Main/>
-    
-    
-    <div>
-      
-    </div>
   </div>
 </template>
 
@@ -20,22 +48,125 @@ import Main from './components/Main.vue'
 export default {
   name: 'App',
   components: {
-
     Main
-
+  },
+  data () {
+    return {
+      activeItem: 'clients',
+    }
+  },
+  methods: {
+    isActive (menuItem) {
+      return this.activeItem === menuItem
+    },
+    setActive (menuItem) {
+      this.activeItem = menuItem
+    }
   }
 }
-
-
 </script>
   
 <style>
-/* #app {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+.none {
+  display: none;
+}
+.active {
+  display: block;
+}
+
+.header {
+    width: 100%;
+    height: 80px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #2e394b;
+}
+
+.main_head {
+  padding-left: 20px;
+
+}
+
+.main_head h3{
+  color: #fdfdfd;
+  font-weight: 900;
+}
+
+#navDropdown {
+    width: 250px;
+    height: 60px;
+    margin-right: 50px;
+    padding: 10px;
+    border: 2px solid black;
+    border-radius: 5px;
+    outline: none;
+    background-color: #2e394b;
+    color: #fff;
+}
+
+.tab {
+  overflow: hidden;
+  border-bottom: 1px solid #234063;
+  background-color: #4b87d0;
+}
+
+.tab ul {
+  list-style: none;
+}
+
+.tab ul li button {
+  background-color: inherit;
+  color: #a1a1a1;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  font-weight: 700;
+  transition: 0.3s;
+}
+
+.tab ul li button:hover {
+  background-color: #ddd;
+}
+
+.tab button.active {
+  color: #e7e7e7;
+}
+
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
+}
+
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,38 +1,10 @@
 <template>
     <div>
 
-        <div class="header">
-            <div>
-                <h3>TakeLessons</h3>
-            </div>
-            
-            
-            <div>
-                <select name="cars" id="cars">
-                    <option value="volvo">Alphabetical</option>
-                    <option value="saab">By age</option>
-                    <option value="mercedes">Mercedes</option>
-                    <option value="audi">Audi</option>
-                </select> 
-            </div>     
-        </div>
-
-        <div class="nav-bar">
-            <div>
-                <b-tabs content-class="mt-3" >
-                    <b-tab title="DASHBOARD"><p>I'm the first tab</p></b-tab>
-                    <b-tab title="CALENDAR"><p>I'm the second tab</p></b-tab>
-                    <b-tab title="CLIENTS" active><p>I'm the tab with the very, very long title</p></b-tab>
-                    <b-tab title="FINANCES"><p>I'm a disabled tab!</p></b-tab>
-                </b-tabs>
-            </div>
-        </div>
-
-
         <div class="contain">
             <div class="sidebar">
                 <div class="add_new">
-                    <button>+ New Contact</button>
+                    <button >+ New Contact</button>
                     <form role="search" id="form">
                         <input type="search"  v-model="search"
                         placeholder="Search...">
@@ -41,9 +13,10 @@
   
                 <div class="sort_dropdown">
                     <label for="sort_contacts">Sort by:</label>
-                    <select name="sort_contacts" id="sort_contacts" v-model="sort">
-                        <option @click="sortName()" value="name">name</option>
-                        <option @click="sortAge()" value="age">age</option>
+                    <select name="sort_contacts" id="sort_contacts" v-model="sort" @change="sortDropdown()">
+                        <option disabled value="">Chosee option</option>
+                        <option value="name">name</option>
+                        <option  value="age">age</option>
                     </select>
                 </div>
                 <div class="contacts_list">
@@ -51,46 +24,154 @@
                     <div>
                         <ul class="list">
                             <li   v-for="client in filteredClients" :key="client"   @click="showClient(client) ">
-                                <img :src="require(`../assets/${client.image}`)" width="40" height="40" alt class="icon" />                                
-                                <a id="set" href="#">{{client.name}}</a>   
+                                <img :src="require(`../assets/${client.image}`)" width="60" height="60" alt class="icon" />                                
+                                <span id="set">{{client.name}}</span>  
                             </li>
                         </ul>
                     </div>
                 </div>        
 
             </div>
-            <div class="client_info">
+            <div class="client_container">
                 <div class="client_name">
-                    <p>{{selectedClient.name}}</p>
+                    <p class="large_text">{{selectedClient.name}}</p>
                 </div>
-                <div class="flex">
-                    <div class="client_notes" style="width: 30%;">
-                        <p>{{selectedClient.name}}</p>
-                        <img :src="require(`../assets/${selectedClient.image}`)"  alt class="icon" />
-                        <p>Notes</p>
-                        <p>{{ selectedClient.note}}</p>
+                <div class="flex flex_column">
+                    <div class="client_notes">
+                        <div class="image">
+                            <img :src="require(`../assets/${selectedClient.image}`)"  alt class="icon" />
+                        </div>
+                        <p class="small_text">Notes</p>
+                        <p class="note_text">{{ selectedClient.note}}</p>
                     </div>
                     <div class="client_info">
-                        <div class="flex">
-                            <div>{{ selectedClient.sex}}</div>
-                            <div>{{ selectedClient.age}}</div>
-                            <div>{{ selectedClient.birthday}}</div>
+                        <div class="gap_small flex">
+                            <div class="col_sex">
+                                <p class="small_text"> Sex</p>
+                                <p class="client_data_text">{{ selectedClient.sex}}</p>
+                            </div>
+                            <div class="col_age">
+                                <p class="small_text">Age</p>
+                                <p class="client_data_text">{{ selectedClient.age}}</p>
+                            </div>
+                            <div class="col_bday">
+                                <p class="small_text">Birthday</p>
+                                <p class="client_data_text">{{ selectedClient.birthday}}</p>
+                            </div>
                         </div>
                         <div>
-                            <div>{{ selectedClient.adress}}</div>
+                            <div>
+                                <p class="small_text">Adress</p>
+                                <p class="client_data_text">{{ selectedClient.adress}}</p>
+                            </div>
                         </div>
                         <div class="flex">
-                            <div>{{ selectedClient.cell}}</div>
-                            <div>{{ selectedClient.email}}</div>    
+                            <div class=" gap_large">
+                                <p class="small_text">Home</p>
+                                <p class="client_data_text">{{ selectedClient.cell}}</p>
+                            </div>
+                            <div class=" gap_large">
+                                <p class="small_text">Primary Email</p>
+                                <p class="client_data_text">{{ selectedClient.email}}</p>
+                            </div>    
                         </div>
                         <div class="flex">
-                            <div>{{ selectedClient.cell}}</div>
-                            <div>{{ selectedClient.email}}</div>    
+                            <div class=" gap_large">
+                                <p class="small_text">Cell</p>
+                                <p class="client_data_text">{{ selectedClient.cell}}</p>
+                            </div>
+                            <div class=" gap_large">
+                                <p class="small_text">Secondary Email</p>
+                                <p class="client_data_text">{{ selectedClient.email}}</p>
+                            </div>    
                         </div>     
                     </div>
                 </div>
+                <div class="schedule">
+                    <p class="client_data_text">Schedule</p> 
+                </div>
+                <div class="lessons_container">
+                    <div class="lesson lesson_border">
+                        <div class="flex space_between border_bott padding">
+                            <div class="lesson_date_text"><p>Tueseday 9:00am <span>60 / Weekly</span></p></div>
+                            <div>
+                                <select name="Action" id="" class="lesson_select">
+                                    <option value="action" active>Chose an action</option>
+                                    <option value="">Action</option>
+                                    <option value="">Action</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="flex gap_small padding">
+                            <div>
+                                <p class="small_text">Service</p>
+                                <p class="client_data_text">Switch Flip 101</p>
+                            </div>
+                            <div>
+                                <p class="small_text">Location</p>
+                                <p class="client_data_text">Fantasy Fa...</p>
+                            </div>
+                            <div>
+                                <p class="small_text">Status</p>
+                                <p class="client_data_text">Paid</p>
+                            </div>
+                        </div>
+                    </div>
 
-                                     
+                    <div class="lesson">
+                        <div class="flex space_between border_bott padding">
+                            <div class="lesson_date_text"><p>Tueseday 9:00am <span>60 / Weekly</span></p></div>
+                            <div>
+                                <select name="Action" id="" class="lesson_select">
+                                    <option value="action" active>Chose an action</option>
+                                    <option value="">Action</option>
+                                    <option value="">Action</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="flex gap_small padding">
+                            <div>
+                                <p class="small_text">Service</p>
+                                <p class="client_data_text">Mega Ramp</p>
+                            </div>
+                            <div>
+                                <p class="small_text">Location</p>
+                                <p class="client_data_text">The Berricks</p>
+                            </div>
+                            <div>
+                                <p class="small_text">Service</p>
+                                <p class="client_data_text">Canceled</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="lesson">
+                        <div class="flex space_between border_bott padding">
+                            <div class="lesson_date_text"><p>Tueseday 9:00am    <span>60 / Weekly</span></p></div>
+                            <div>
+                                <select name="Action" id="" class="lesson_select">
+                                    <option value="action" active>Chose an action</option>
+                                    <option value="">Action</option>
+                                    <option value="">Action</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="flex gap_small padding">
+                            <div>
+                                <p class="small_text">Service</p>
+                                <p class="client_data_text">S.K.A.T.E Sess..</p>
+                            </div>
+                            <div>
+                                <p class="small_text">Service</p>
+                                <p class="client_data_text">The Berricks</p>
+                            </div>
+                            <div>
+                                <p class="small_text">Service</p>
+                                <p class="client_data_text">Paid</p>
+                            </div>
+                        </div>
+                    </div>                    
+                </div>                         
             </div>
         </div>
     </div>
@@ -105,61 +186,153 @@
                 clients: contacts,
                 selectedClient: contacts[0],
                 search: '',
-                sort: 'name',
-                red: 'red'
-                
+                sort: '',                
             }
         },
         methods: {
             showClient(client) {
                 this.selectedClient = client;
                 console.log(this.sort)
-            }          
+            },
+            sortDropdown: function() {
+                if(this.sort === 'name'){
+                    this.clients.sort((a,b) => {
+                        if(a.name < b.name) { return -1; }
+                        if(a.name > b.name) { return 1; }
+                        return 0;
+                    })
+                }
+                else {
+                  return  this.clients.sort((a, b)=> a.age - b.age)
+                }
+            }
         },
         computed: {
+
             filteredClients () {
                 return this.clients.filter((client) => {
                     return client.name.toLowerCase().match(this.search.toLowerCase())
                 })
             },
+
         }
     }   
 </script>
 
 <style scoped>
+
 .flex {
     display: flex;
 }
-
-
-.header {
-    width: 100%;
-    height: 80px;
-    display: flex;
-    flex-direction: row;
+.space_between {
     justify-content: space-between;
-    align-items: center;
-    background-color: #2e394b;
 }
 
-#cars {
-    width: 250px;
-    height: 60px;
-    margin-right: 50px;
-    padding: 10px;
-    border: 2px solid black;
+.border_bott {
+    border-bottom: 1px solid #aaa;
+}
+
+.col_age { width: 20%; }
+.col_sex { width: 30%; }
+.col_bday { width: 50%; }
+.gap_large { width: 50%; }
+
+.lesson {
+    width: 90%;
+    margin: 20px auto;
+    border: 1px solid #aaa;
     border-radius: 5px;
-    outline: none;
-    background-color: #2e394b;
-    color: #fff;
+    border-left: 15px solid #f27a0a;
+}
+
+.lesson_border {
+    border-left: 15px solid #fec506;
+}
+
+.lesson_date_text p{
+   margin: 0;
+   font-size: 22px;
+   font-weight: 700;
+   color: #535252; 
+}
+
+.lesson_date_text p span {
+    color: #c9cdd0;
+}
+
+.lesson_select {
+    padding: 10px 10px;
+    width: 250px;
+    border-radius: 3px;
+    background-color: #e7e7e7;
+}
+
+.padding {
+    padding: 30px;
+}
+
+.image {
+    width: 100%;
+    margin-bottom: 10px;
+    height: 400px;
+    overflow: hidden;
+}
+
+.image img {
+    width: 100%;
+    min-height: 300px;
+}
+
+.note_text {
+    font-weight: 500;
+    font-size: 20px;
+    text-align: left;
+    color: #727272;
+    word-spacing: 2px;
+    width: 90%;
+}
+
+.small_text {
+    color: #bbbbbb;
+    margin-bottom: 10px;
+    font-weight: 500;
+    font-size: 18px;
+}
+.large_text {
+    color: #929292;
+    font-weight: 500;
+    font-size: 50px;
+    margin-left: 20px;
+    letter-spacing: 2px;
+    /* font-weight: bold; */
+}
+.client_data_text {
+    font-weight: 700;
+    font-size: 22px;
+    margin-top: 5px;
+    color: #535252;
+}
+.client_notes {
+    width: 30%;
+    margin-left: 20px;
+}
+.client_info{
+    width: 40%;
+    margin-left: 20px;
+}
+.client_container {
+    width: 75%;
 }
 
 .sidebar {
-    width: 20%;
-    border-right: 1px solid #aaa;
+    width: 25%;
+    border-right: 1px solid #cacaca;
 }
-.client_info {
-    width: 60%;
+
+.contacts_list {
+    height: 100%;
+    max-height: 100vh;
+    overflow: auto;
 }
 
 .contain {
@@ -167,20 +340,26 @@
 }
 
 .add_new {
-    padding: 10px;
+    padding: 30px 10px;
+    background-color: #fafafa;
 }
 
 .add_new button {
     width: 100%;
     background-color: #5cb35b;
-    border: 1px solid #000;
+    border: 1px solid #a7a7a7;
     border-radius: 2px;
     padding: 10px;
     margin-bottom: 10px;
     cursor: pointer;
     color: #fff;
     font-size: 17px;
-  }
+    transition: 0.2s ease-in;
+}
+.add_new button:hover {
+    background-color: #fff;
+    color: #5cb35b;
+}
   #form {
     background-color: #fff;
     border: 1px solid #aaa;
@@ -212,6 +391,7 @@
     width: 25px;
     height: 44px;
   }
+  
   
   #form svg {
     color: #aaa;
@@ -258,6 +438,7 @@
     background-color: #fafafa;
     border-bottom: 1px solid #d8d8d8;
     cursor: pointer;
+    transition: 0.2s ease-in;
 }
 .list li:hover {
     background-color: #d8d8d8;
@@ -270,5 +451,36 @@
 
 .list li img {
     margin-right: 20px;
+}
+
+.schedule {
+    background-color: #ebebeb;
+    height: 60px;
+    padding: 10px;
+    border-top: 1px solid #cacaca;
+    border-bottom: 1px solid #cacaca;
+}
+@media only screen and (max-width: 1440px) {
+    .image {
+        height: 250px;
+    }
+}
+@media only screen and (max-width: 768px) {
+    .flex_column {
+        flex-direction: column;
+    }
+    .client_notes {
+        width: 80%;
+    }
+    .client_info {
+        width: 80%;
+    }
+}
+
+
+.col {
+    -webkit-box-flex: 0 !important;
+    -ms-flex: 0 0 50% !important;
+     flex: 0 0 50% !important;
 }
 </style>
